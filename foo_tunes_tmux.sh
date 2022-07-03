054d2471e8f081b303dda779f5b91353d8318f97
James Nguyen <james@jojojames.com>
2022-07-03 15:59:52 -0700

    Add tmux script

---
+++ foo_tunes_tmux.sh
@@ -0,0 +1,12 @@
+#!/usr/local/bin/bash
+
+PATH=/usr/local/bin
+
+if ! tmux has-session -t footunes 2>/dev/null; then
+    # https://stackoverflow.com/questions/25207909/tmux-open-terminal-failed-not-a-terminal
+    tmux new-session -d -s footunes python /bebe/script/foo_tunes/foo_tunes.py \
+         --jojo \
+         --watch_sleep_time=30 \
+         --watch_playlist_delay=30 \
+         --watch_convert_delay=300 # Five minutes.
+fi
