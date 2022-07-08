6bae9648b52d0d9a9b388c737c9780637706a0fa
James Nguyen <james@jojojames.com>
2022-07-07 21:37:19 -0400

    Update how long we wait

--- foo_tunes_tmux.sh
+++ foo_tunes_tmux.sh
@@ -7,6 +7,6 @@ if ! tmux has-session -t footunes 2>/dev/null; then
     tmux new-session -d -s footunes python /bebe/script/foo_tunes/foo_tunes.py \
          --jojo \
          --watch_sleep_time=30 \
-         --watch_playlist_delay=30 \
-         --watch_convert_delay=300 # Five minutes.
+         --watch_playlist_delay=25 \
+         --watch_convert_delay=180 # Three minutes.
 fi
